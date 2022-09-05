0451ada8a03f16de316ac482f39dfb92b141a410
Mithun <mithunnit@gmail.com>
2022-09-05 11:23:42 +1000

    change to welcome page

--- app/src/main/java/hello/HelloController.java
+++ app/src/main/java/hello/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Hello from Spring Boot application!!";
+        return "Hello from Spring Boot application";
     }   
 }
